362284948ad1c624bbcfb41196ea3f2bd4860562
Khenende Netshivhambe <ST10379469@vcconnect.edu.za>
2025-11-21 02:59:10 +0200

    Updated Program.csUpdated ViewsUpdated _layout.cshtml

---
+++ ProgPoe2/Models/ClaimRepository.cs
@@ -0,0 +1,7 @@
+namespace ProgPoe2.Models
+{
+    public static class ClaimRepository
+    {
+        public static List<Claim> Claims { get; set; } = new List<Claim>();
+    }
+}
---
+++ ProgPoe2/Models/HRReport.cs
@@ -0,0 +1,17 @@
+﻿namespace ProgPoe2.Models
+{
+    public class HRReport
+    {
+        public DateTime ReportDate { get; set; } = DateTime.Now; // The date the report was generated
+        public List<Claim> Claims { get; set; } // List of claims included in the report
+        public int TotalClaims { get; set; } // Total number of claims
+        public decimal TotalAmount { get; set; } // Total amount to be paid for all approved claims
+        public int ApprovedClaimsCount { get; set; } // Count of approved claims
+        public int PendingClaimsCount { get; set; } // Count of pending claims
+        public int RejectedClaimsCount { get; set; } // Count of rejected claims
+        //Troelsen and Japikse (2021)
+    }
+}
+/*Reference List
+Troelsen, A. and Japikse, P. 2021. Pro C# 9 with .NET 5: Foundational Principles and Practices in Programming. 10th ed. Apress
+*/
